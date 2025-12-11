c0a9666e7331c8a60b2b7de659f33242247fecf8
Chance Anderson <ccanderson5@wisc.edu>
2025-12-10 19:46:53 -0600

    Added VenmoQR and Favi Image

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -1,11 +1,11 @@
-{/*import venmoQR from "../assets/venmoQR.png"*/}
+import venmoQR from "../assets/venmoQR.jpg";
 
 export default function Donate(){
     return (
         <div>
             <h1>Scan to Donate</h1>
             <p>Donations of any amount help us fund special events for the club!</p>
-            
+            <img src={venmoQR} alt="PRSA venmo QR code to donate"/>
         </div>
     );
 
